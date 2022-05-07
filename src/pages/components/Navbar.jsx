fd0f09f40a6692c17bc2d9f7f554f02bedb5a8a5
Septiana Yoga <septianayoga30@gmail.com>
2022-05-07 08:43:52 +0700

    initial navbar with styledcomponents

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styledComponents from "styled-components";
+
+const Container = styledComponents.div`
+  height: 60px;
+`;
+
+const Wrapper = styledComponents.div`
+  padding: 10px 20px;
+`;
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Wrapper>Navbar</Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
